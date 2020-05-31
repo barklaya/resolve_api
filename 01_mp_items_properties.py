58bd882c943180fbd58b2b7603331324dcab8a3a
barklaya <barklaya@email.cz>
2020-05-31 15:53:26 +0200

    Added 01_mp_items_properties.py script file

---
+++ 01_mp_items_properties.py
@@ -0,0 +1,28 @@
+import DaVinciResolveScript as dvr
+resolve = dvr.scriptapp("Resolve")
+
+"""
+A small script to get all the metadata from items in a root folder (media as timelines, images ect. are considered as clips by default)
+
+pm == project manager
+proj == project
+tl == timeline
+"""
+
+pm = resolve.GetProjectManager()
+pm.LoadProject("RESOLVE_API_TEST")
+proj = pm.GetCurrentProject()
+mediapool = proj.GetMediaPool()
+rootFolder = mediapool.GetRootFolder()
+resolve_media_storage = resolve.GetMediaStorage()
+clips = rootFolder.GetClipList()
+
+for clip in clips:
+    # from IPython import embed
+    # embed()
+    clip_porerties = clip.GetClipProperty()
+    clipname = clip.GetName()
+    print("\n" + "------------------" + "\n" +
+          clipname + "\n" + "------------------")
+    for key, value in clip_porerties.items():
+        print(key, ':', value)
